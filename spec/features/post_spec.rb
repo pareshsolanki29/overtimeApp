636f2a336092541985cda39e5a792ab3fe26ac58
pareshsolanki29 <bandralites@gmail.com>
2017-12-20 13:28:26 -0500

    -mnew posts and rspec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -17,4 +17,18 @@ describe 'navigate' do
       end 
 	end
 
+		 describe 'creation ' do
+		 	it 'has a new form which can be reached' do
+		 		visit new_post_path
+		 		expect(page.status_code).to eq(200)
+		 	end 
+		 	it 'can be created from new form page' do
+		 		visit new_post_path
+		 		fill_in 'post[date]', with: Date.today
+		 		fill_in 'post[rationale]', with: "Some rationale"
+
+		 		click_on "Save"
+		 		expect(page).to have_content("Some rationale")
+		 	end 
+		 end 
 end
